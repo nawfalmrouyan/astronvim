00dd57e9f48274716621e1c83f633497694e69be
Nawfal <nawfalmrouyan@gmail.com>
2024-05-14 19:42:31 +0800

    update plugin options

---
+++ lua/plugins/codesnap-nvim.lua
@@ -0,0 +1,15 @@
+return {
+  "mistricky/codesnap.nvim",
+  opts = {
+    mac_window_bar = true,
+    title = "Snapshot",
+    code_font_family = "PragmataPro Nerd Font Mono",
+    watermark = "",
+    bg_theme = "bamboo",
+    breadcrumbs_separator = "/",
+    has_breadcrumbs = true,
+    has_line_number = true,
+    save_path = "~/Screenshots",
+    min_width = 0,
+  },
+}
