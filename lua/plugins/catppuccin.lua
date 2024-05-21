7a3b0cc928bd56ebdbadb81ef576f9086375f605
Nawfal <nawfalmrouyan@gmail.com>
2024-05-21 18:24:13 +0800

    setup undercurl

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -13,6 +13,21 @@ return {
         overseer = true,
         lsp_trouble = true,
         rainbow_delimiters = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "undercurl" },
+            -- hints = { "undercurl" },
+            warnings = { "undercurl" },
+            -- information = { "undercurl" },
+          },
+        },
       },
     },
   },
