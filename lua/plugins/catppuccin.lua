a457d6c1538029ad268175503aafc14ad2deb391
Nawfal <nawfalmrouyan@gmail.com>
2024-05-21 15:53:03 +0800

    reenable after fixed

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       integrations = {
         sandwich = false,
-        -- noice = true,
+        noice = true,
         mini = true,
         leap = true,
         markdown = true,
