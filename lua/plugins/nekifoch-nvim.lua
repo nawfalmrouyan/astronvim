c4cecbdace10691c02eb667a06404924651abfda
Nawfal <nawfalmrouyan@gmail.com>
2024-06-04 11:26:09 +0800

    change kitty font on-the-fly

---
+++ lua/plugins/nekifoch-nvim.lua
@@ -0,0 +1,10 @@
+return {
+  "NeViRAIDE/nekifoch.nvim",
+  cmd = "Nekifoch",
+  opts = {
+    kitty_conf_path = vim.fn.expand "~/.config/kitty/kitty.conf",
+    which_key = {
+      enable = true,
+    },
+  },
+}
