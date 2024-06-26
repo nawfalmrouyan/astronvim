fb31df73144a696714d228045739bc132a18e5ef
Nawfal <nawfalmrouyan@gmail.com>
2024-06-26 21:35:50 +0800

    disable redundant config. disable telepath plugin

--- lua/plugins/bigfile-nvim.lua
+++ lua/plugins/bigfile-nvim.lua
@@ -1,3 +1,4 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "LunarVim/bigfile.nvim",
   event = { "FileReadPre", "BufReadPre", "User FileOpened" },
--- lua/plugins/telepath.lua
+++ lua/plugins/telepath.lua
@@ -1,10 +1,10 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 return {
   {
     "rasulomaroff/telepath.nvim",
     dependencies = "ggandor/leap.nvim",
     lazy = false,
-    config = function()
-      require("telepath").use_default_mappings()
-    end,
+    config = function() require("telepath").use_default_mappings() end,
   },
 }
