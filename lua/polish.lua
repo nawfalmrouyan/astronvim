6395921b98ce381590ca3bb2c5b4ff85b5d49efc
Nawfal <nawfalmrouyan@gmail.com>
2024-06-28 18:53:11 +0800

    make pasting image using C-a only works in insert mode

--- lua/polish.lua
+++ lua/polish.lua
@@ -182,7 +182,7 @@ vim.keymap.set("n", "<leader>fX", '<cmd>!chmod -x "%"<CR>', { silent = true, des
 -- I use a Ctrl keymap so that I can paste images in insert mode
 -- I tried using <C-v> but duh, that's used for visual block mode
 -- so don't do it
-vim.keymap.set({ "n", "v", "i" }, "<C-a>", function()
+vim.keymap.set({ "i" }, "<C-a>", function()
   -- Call the paste_image function from the Lua API
   -- Using the plugin's Lua API (require("img-clip").paste_image()) instead of the
   -- PasteImage command because the Lua API returns a boolean value indicating
