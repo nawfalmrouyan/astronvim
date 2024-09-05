d94828820f9d3fc0c75ec0482e84962f48ed9d46
Nawfal <nawfalmrouyan@gmail.com>
2024-09-05 21:41:41 +0800

    use webview for peek.nvim

--- lua/plugins/peek-nvim.lua
+++ lua/plugins/peek-nvim.lua
@@ -1,6 +1,7 @@
 return {
   "toppair/peek.nvim",
   opts = {
-    app = { "/opt/zen-browser-optimized-bin/zen-bin", "--new-window" },
+    -- app = { "/opt/zen-browser-optimized-bin/zen-bin", "--new-window" },
+    app = "webview",
   },
 }
