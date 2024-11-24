62d9975e58772eb28a2cc4fa02fdd80286192700
OpalBSD <nawfalmrouyan@gmail.com>
2024-11-24 21:19:42 +0800

    s/nvim-surround/mini-surround/

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -16,7 +16,7 @@ return {
       end,
       integrations = {
         blink_cmp = true,
-        nvim_surround = true,
+        -- nvim_surround = true,
         render_markdown = true,
         dadbod_ui = true,
         native_lsp = {
