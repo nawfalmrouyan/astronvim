1b539b806bb4825655bfd2cc35b37f2c159bbdd7
OpalBSD <nawfalmrouyan@gmail.com>
2024-11-26 15:13:43 +0800

    make nvim-notify not annoying.

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -112,5 +112,13 @@ return {
   },
 
   { "dstein64/vim-startuptime" },
+  { "nvim-notify",
+  opts = {
+      render = "wrapped-compact",
+      stages = "fade_in_slide_out",
+      timeout = 3000,
+      top_down = false
+    }
+  },
   -- { "markview.nvim", ft = { "markdown", "codecompanion" } },
 }
