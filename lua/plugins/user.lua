8bb26019e8b237a2b62a3d6ab8758f5f0bb57eba
OpalBSD <nawfalmrouyan@gmail.com>
2024-12-18 22:25:04 +0800

    use blink

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -175,4 +175,15 @@ return {
       },
     },
   },
+  -- {
+  --   "saghen/blink.cmp",
+  --   opts = {
+  --     keymap = { preset = "super-tab" },
+  --     appearance = {
+  --       use_nvim_cmp_as_default = true,
+  --       nerd_font_variant = "normal",
+  --     },
+  --     signature = { enabled = true },
+  --   },
+  -- },
 }
