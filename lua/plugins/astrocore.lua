f38ee8c4e323f967be1a20cba582b57107e3d151
OpalBSD <nawfalmrouyan@gmail.com>
2024-12-18 23:14:01 +0800

    enable spelling?

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -66,7 +66,7 @@ return {
       opt = { -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
+        spell = true, -- sets vim.opt.spell
         signcolumn = "auto", -- sets vim.opt.signcolumn to auto
         ignorecase = false,
         smartcase = false,
