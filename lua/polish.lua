1908cf1dd9396e3d5c7279467f59da7f5bb25d0a
OpalBSD <nawfalmrouyan@gmail.com>
2024-12-21 09:28:30 +0800

    remove commented configs

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,9 +1,3 @@
--- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
-
 -- Set up custom filetypes
 vim.filetype.add {
   extension = {
@@ -22,19 +16,14 @@ vim.filetype.add {
   },
 }
 
-vim.opt.guicursor = {
-  "n-v:block-block-Cursor/lCursor",
-  "i-c-ci-ve:ver25-Cursor/lCursor",
-  "r-cr:hor20",
-  "o:hor50",
-  "i:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
-  "sm:block-blinkwait175-blinkoff150-blinkon175",
-}
+-- vim.opt.guicursor = {
+--   "n-v:block-block-Cursor/lCursor",
+--   "i-c-ci-ve:ver25-Cursor/lCursor",
+--   "r-cr:hor20",
+--   "o:hor50",
+--   "i:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
+--   "sm:block-blinkwait175-blinkoff150-blinkon175",
+-- }
 
 require "neovide"
 require "keymaps"
-
--- Call theme
--- require("base46").theme = "catppuccin"
--- vim.cmd "Nvdash"
--- vim.cmd "colorscheme catppuccin-mocha"
