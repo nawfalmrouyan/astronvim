c44af9936021175ba0dd21c6edefc9a16f7cef8e
OpalBSD <nawfalmrouyan@gmail.com>
2024-12-30 12:01:42 +0800

    add smear-cursor (neovide like cursor) plugin

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,19 @@
+return {
+  "sphamba/smear-cursor.nvim",
+
+  opts = {
+    -- Smear cursor when switching buffers or windows.
+    smear_between_buffers = true,
+
+    -- Smear cursor when moving within line or to neighbor lines.
+    smear_between_neighbor_lines = true,
+
+    -- Draw the smear in buffer space instead of screen space when scrolling
+    scroll_buffer_space = true,
+
+    -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+    -- Smears will blend better on all backgrounds.
+    legacy_computing_symbols_support = false,
+    -- transparent_bg_fallback_color = "#303030",
+  },
+}
