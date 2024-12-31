e05e2247b2ce17cc9fc2b2fa5509ce8f320c8a73
OpalBSD <nawfalmrouyan@gmail.com>
2025-01-01 01:16:12 +0800

    reenable de/indent maps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,8 @@
 -- clear windows newline
 vim.keymap.set("n", ",m", function() vim.cmd ":%s/\r//g" end)
 
--- vim.keymap.set("n", "<", "<<")
--- vim.keymap.set("n", ">", ">>")
+vim.keymap.set("n", "<", "<<")
+vim.keymap.set("n", ">", ">>")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 -- better up/down
