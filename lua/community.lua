42880b854c1bddee30ecf028d1322f07615b79e5
OpalBSD <nawfalmrouyan@gmail.com>
2025-02-04 17:42:04 +0800

    enable htmlcss and ansible pack

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,12 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.bash" },
   -- { import = "astrocommunity.pack.docker" },
   -- { import = "astrocommunity.pack.full-dadbod" },
   { import = "astrocommunity.pack.go" },
-  -- { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
